a37df03d3d4e144d283a0ec2d04b045493c13c9a
Daniel WIllson <daniel@danielwillson.me>
2016-12-21 22:11:33 -0700

    Initial commit of website files from building on the plane

---
+++ main.js
@@ -0,0 +1,9 @@
+function showHide() {
+	var disp = document.getElementById('check').style.display;
+	if (disp == "block") {
+		document.getElementById('check').style.display='';
+	}
+	else {
+		document.getElementById('check').style.display='block';
+	}
+}
