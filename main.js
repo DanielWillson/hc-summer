7ffdd434b90b1c36d9cba0d6b305aa22a3da9d9d
Daniel WIllson <daniel@danielwillson.me>
2016-12-26 20:57:09 -0500

    Responsive fixes for the .top and .learn-more divs with 100% vh heights

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+	var vh = $(".vh").height() + 60;
+	console.log(vh);
+	$(".vh").height(vh);
+	$(".filter").height(vh);
+	console.log("test");
+};
+
 function showHide() {
 	var disp = document.getElementById('check').style.display;
 	if (disp == "block") {
